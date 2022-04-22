d59f4fa44fcecf9a5586d53134407f2b908e8725
纯天然疯子 <101387102+code-zdf@users.noreply.github.com>
2022-04-22 13:56:53 +0800

    VS

---
+++ SAMS/SAMS/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMS
+{
+    internal class Data
+    {
+        public static string UID = "", UName = "";//登录用户的ID和姓名
+    }
+}
